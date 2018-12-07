ce15852d4dec1a6af34e9242b437f41c0559f80f
smartlijunyi <45683008+smartlijunyi@users.noreply.github.com>
2018-12-07 16:39:59 +0800

    Add files via upload

---
+++ nodeValue.c
@@ -0,0 +1,23 @@
+#include "math.h"
+
+#include "quadtreeStructure.h"
+
+// Evaluate function at centre of quadtree node
+
+double nodeValue( Node *node, double time ) {
+
+  int level = node->level;
+  double x = node->xy[0];
+  double y = node->xy[1];
+
+  double h = pow(2.0,-level);
+
+  return( value( x+0.5*h, y+0.5*h, time ) );
+}
+
+// Data function
+
+double value( double x, double y, double time ) {
+
+  return( 2.0*exp(-8.0*(x-time)*(x-time)) - 1.0 ) ;
+}
